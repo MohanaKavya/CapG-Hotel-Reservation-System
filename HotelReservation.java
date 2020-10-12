3875fc127c96936d6563624a8495ee1e3ca5d2f2
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-12 22:41:12 +0530

    Added : Welcome Message Added

---
+++ HotelReservation.java
@@ -0,0 +1,13 @@
+package com.capgemini.assignment.hotelreservation;
+
+public class HotelReservation {
+
+	public HotelReservation() {
+	}
+
+	public static void main(String[] args) {
+		// Display Message
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
